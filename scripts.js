31648c2f6bb0a2a0c6bd0d1fe95db59d023481c2
shock59 <42130427+shock59@users.noreply.github.com>
2022-06-23 10:30:40 +1000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(400, 400);
+}
+  
+function draw() {
+    background("#000000");
+    fill("#ff0000");
+    strokeWeight(0);
+    rect(100, 100, 200, 150, 40, 40, 0, 0);
+    rect(100, 250, 80, 70);
+    rect(220, 250, 80, 70);
+    rect(250, 160, 80, 70, 20);
+    fill("#398bad");
+    rect(100, 160, 60, 60);
+}
